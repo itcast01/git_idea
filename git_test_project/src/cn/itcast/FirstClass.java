1218b967973add380ae7fe0af8449397dd15ee7b
zj <12992276@qq.com>
2019-02-21 13:15:12 +0800

    添加一个文件到本地仓库

---
+++ git_test_project/src/cn/itcast/FirstClass.java
@@ -0,0 +1,16 @@
+package cn.itcast;
+
+/**
+ * @author 黑马程序员:TTC130
+ * @Company http://www.ithiema.com
+ */
+public class FirstClass {
+    public static void main(String[] args) {
+        System.out.println("哈哈哈哈哈哈哈或");
+        System.out.println("哈哈哈哈哈哈哈或");
+        System.out.println("哈哈哈哈哈哈哈或");
+        System.out.println("哈哈哈哈哈哈哈或");
+        System.out.println("哈哈哈哈哈哈哈或");
+        System.out.println("哈哈哈哈哈哈哈或");
+    }
+}
